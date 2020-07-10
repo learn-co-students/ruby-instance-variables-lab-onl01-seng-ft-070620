e7c2fb59f19302c90bef300c003e339aa18dec05
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-10 12:29:01 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+## 1. create a Dog class
+## 2. create an instance method #name= which is a setter (writer)
+## 3. create an instance method #name which is a getter (reader)
+
+class Dog
+
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end 
+    
+    def name
+        @this_dogs_name
+    end 
+
+end 
+
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+lassie.name 
