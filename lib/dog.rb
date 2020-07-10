7757841ab75a64edb133676c0429861f078c25b7
Doug Schallmoser <dougschallmoser@mgail.com>
2020-07-10 12:29:45 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,8 @@
 ## 1. create a Dog class
 ## 2. create an instance method #name= which is a setter (writer)
 ## 3. create an instance method #name which is a getter (reader)
+## 4. be sure to use instance variables and not local
+##      variables for #name= and name
 
 class Dog
 
